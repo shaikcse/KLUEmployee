ea0004345f09cf52c76234afea8d4ef550a4d32e
shaikcse <shaikshavali43@gmail.com>
2018-05-04 12:15:00 +0530

    modified 2 lines by replaceing kname,kemployee

--- empsalary.c
+++ empsalary.c
@@ -2,8 +2,8 @@
  #include <stdio.h>
  /*structure declaration*/
 struct employee{
-    char    name[30];
-    int     empId;
+    char    kname[30];
+    int    kempId;
     float   salary;
 };
  int main()
@@ -13,14 +13,14 @@ struct employee{
      
     /*read employee details*/
     printf("\nEnter details :\n");
-    printf("Name ?:");          gets(emp.name);
-    printf("ID ?:");            scanf("%d",&emp.empId);
+    printf("Name ?:");          gets(emp.kname);
+    printf("ID ?:");            scanf("%d",&emp.kempId);
     printf("Salary ?:");        scanf("%f",&emp.salary);
      
     /*print employee details*/
     printf("\nEntered detail is:");
-    printf("Name: %s"   ,emp.name);
-    printf("Id: %d"     ,emp.empId);
+    printf("Name: %s"   ,emp.kname);
+    printf("Id: %d"     ,emp.kempId);
     printf("Salary: %f\n",emp.salary);
     return 0;
  }
